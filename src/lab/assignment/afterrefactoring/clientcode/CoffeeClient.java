07da11ac01ea517a97de49bf6a08cf2681eec11e
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-02-28 15:20:15 -0800

    template lab

--- src/lab/assignment/afterrefactoring/clientcode/CoffeeClient.java
+++ src/lab/assignment/afterrefactoring/clientcode/CoffeeClient.java
@@ -1,19 +1,16 @@
-package lab.assignment.beforerefactoring.clientcode;
-
-import lab.assignment.afterrefactoring.CoffeeMaker;
-import lab.assignment.afterrefactoring.Mocha;
-import lab.assignment.afterrefactoring.Cappucino;
+package lab.assignment.afterrefactoring.clientcode;
 import lab.assignment.afterrefactoring.Americano;
+import lab.assignment.afterrefactoring.Mocha;
+import lab.assignment.afterrefactoring.CoffeeMaker;
+import lab.assignment.afterrefactoring.Cappucino;
 
 public class CoffeeClient {
-
 
     public static void main(String[] args) {
 
         CoffeeMaker m = new Mocha();
         CoffeeMaker c = new Cappucino();
         CoffeeMaker a = new Americano();
-
 
         m.prepareCoffee();
         
@@ -25,7 +22,5 @@ public class CoffeeClient {
 
         a.prepareCoffee();
 
-
     }
-
 }
